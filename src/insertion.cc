596fee17dc8c528518e06e09db69e16a45457f12
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-09-23 01:45:48 -0400

    add report

--- src/insertion.cc
+++ src/insertion.cc
@@ -1,29 +1,21 @@
 #include <iostream>
 
-void insertion_sort(int input[], int limit){
+int *insertion_sort(int input[], int limit){
 
 
   int i, j, temp;
 
   for (i=0; i< limit; i++){
-    
     for (j=i ;j>=0; j--){
-      
       if(input[j] < input[j-1]){
-
 	temp = input[j];
 	input[j] = input[j-1];
 	input[j-1] = temp;
-	
       }
     }
   }
 
-
-  for(i = 0; i< limit; i++){
-    std :: cout << input[i];
-  }
-  
+  return input;
 }
 
 
@@ -41,13 +33,10 @@ int main(){
     std :: cin >> input[i];
   }
 
-  //  std :: cout << "*****\n";
-  insertion_sort(input, limit);
-  
-  // int *sorted_array = insertion_sort(input, limit);
+  int* sorted_array = insertion_sort(input, limit);
 
-  // for(i = 0; i< limit; i++){
-  //   std :: cout << sorted_array[i];
-  // }
+  for(i = 0; i< limit; i++){
+    std :: cout << sorted_array[i];
+  }
   
 }
