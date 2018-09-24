2775e672cac8605c5dcb04d70831b5282624e1de
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-09-23 23:33:22 -0400

    update time for insertion sort

--- src/mergesort.cpp
+++ src/mergesort.cpp
@@ -1,63 +1,90 @@
 #include <iostream>
+#include <time.h>
 
-void merge(int a[], int left_low, int left_high, int right_low, int right_high) {
-  
-    int length = right_high-left_low+1;
-    int temp[length];
-    int left = left_low;
-    int right = right_low;
-    
-    for (int i = 0; i < length; ++i) { 
-        if (left > left_high)
-            temp[i] = a[right++];
-        else if (right > right_high)
-            temp[i] = a[left++];
-        else if (a[left] <= a[right])
-            temp[i] = a[left++];
-        else
-            temp[i] = a[right++]; 
-    }
-    
-    for (int i=0; i< length; ++i) 
-        a[left_low++] = temp[i];
-    
+
+int *get_input(int limit){
+
+  int* input = new int[limit];
+
+  int j = 0;
+  for(int i=1000; i>1000-limit; i--){
+    input[j] = i;
+    j+=1;
+  }
+
+  return input;
 }
 
-void merge_sort(int a[], int low, int high) {
-  
-    if (low >= high)
-      return;                         
-    
-    else {
-      int mid = (low + high)/2;
-      merge_sort(a, low, mid);        
-      merge_sort(a, mid+1, high);     
-      merge(a, low, mid, mid+1,high); 
+
+
+void copy_merged_array_into_original(int original[], int merged[], int length, int left_low) {
+    for (int i=0; i< length; ++i)
+        original[left_low++] = merged[i];
+}
+
+void merge(int a[], int left_low, int left_high, int right_low, int right_high) {
+
+    int length = right_high-left_low+1;
+    int merged_array[length];
+    int left = left_low;
+    int right = right_low;
+
+    auto left_array_exhausted = [&left, &left_high]() { return left > left_high;};
+    auto right_array_exhausted = [&right, &right_high]() { return right > right_high;};
+
+    for (int i = 0; i < length; ++i) {
+        if (left_array_exhausted())
+            merged_array[i] = a[right++];
+        else if (right_array_exhausted())
+            merged_array[i] = a[left++];
+        else if (a[left] <= a[right])
+            merged_array[i] = a[left++];
+        else
+            merged_array[i] = a[right++];
     }
-    
+    copy_merged_array_into_original(a,  merged_array, length, left_low);
+
+}
+
+void merge_sort(int numbers[], int low, int high) {
+
+    if (low >= high)
+        return;
+
+    else {
+        int mid = (low + high)/2;
+        merge_sort(numbers, low, mid);
+        merge_sort(numbers, mid+1, high);
+        merge(numbers, low, mid, mid+1, high);
+    }
+
 }
 
 
 int main(){
 
-  int limit, i;
-  
-  std :: cout <<"Enter limit";
-  std :: cin>> limit;
 
-  int* input = new int[limit];
-  
-  std :: cout <<"Enter array to sort";
-  for (i=0; i< limit; i++){
-    std :: cin >> input[i];
-  }
+    int limit;
+    
+    std :: cout << "Enter limit: ";
+    std :: cin >> limit;
 
+    int* input = get_input(limit);
 
-  std :: cout << "Using mergesort";
-  merge_sort(input, 0, limit-1);
- 
-  for(i = 0; i< limit; i++){
-    std :: cout << input[i];
-  }
-  
+    std :: cout << "Sorting using mergesort: ";
+
+    clock_t start = clock();
+
+    merge_sort(input, 0, limit-1);
+
+    clock_t end = clock();
+
+    double cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
+
+    printf("Merge Sort took %f seconds to finish \n", cpu_time_used);
+
+    for(int i = 0; i< limit; i++){
+        std :: cout << input[i] << " ";
+    }
+
 }
