74621ff529b284eff41c167ebe686a41de4f83ee
Sreejith Sreekumar <sreejith2904@gmail.com>
2018-10-30 02:03:48 -0400

    finish project 1 main code

--- src/project1/src/min_max_grouping.cpp
+++ src/project1/src/min_max_grouping.cpp
@@ -35,13 +35,13 @@ int main(){
    */
   int C[M][N];
   int T[M][N];
-  int parent[M][N];
+  // int parent[M][N];
   
   for(int i=0;i<M;i++){
     for(int j=0;j<N;j++){
       C[i][j] = 0;
       T[i][j] = 0;
-      parent[i][j] = 0;
+      // parent[i][j] = 0;
     }
   }
 
@@ -66,7 +66,7 @@ int main(){
     for(int i=j; i<N; i++){
 
       std::vector<int> max_group;
-      std::vector<int> k_indices;
+      // std::vector<int> k_indices;
       // what is the variation of k?
 
       for(int k=j-1; k<i; k++ ){
@@ -79,7 +79,7 @@ int main(){
 
 	int min_element = element1 < sum_element ? element1 : sum_element;
 	max_group.push_back(min_element);
-	k_indices.push_back(k);
+	// k_indices.push_back(k);
 	
       }
 
@@ -87,13 +87,8 @@ int main(){
       int max_element = *std::max_element(max_group.begin(), max_group.end());
       int index = find_index(max_group, max_element);
       
-
-      // std::vector<int>::iterator it = std::find(max_group.begin(), max_group.end(), max_element);
-      std::cout<<"Index of k is: "<<index + 1<<"\n";
-      
-      
       max_group.clear();
-      k_indices.clear();
+      // k_indices.clear();
 
       
       C[j][i] = max_element;
@@ -103,7 +98,8 @@ int main(){
       T[j][i] = index + j;
     }
   }
-  
+
+
   std::cout<<"\n C Matrix \n";
   for(int x=0;x<3;x++){
     for(int y=0;y<12;y++){
@@ -120,9 +116,33 @@ int main(){
     std::cout<<"\n";
   }
 
-  /**
-   *
-   *
-   **/
+
+  std::cout<<"\n"<<"Finding the number of elements in each group\n";
+
+  std::vector<int> increments_in_groups;
+  increments_in_groups.push_back(N);
+
+  int tmp_index = N-1;
+  for(int k=M-1; k>0; k--){
+    int next_index = T[k][tmp_index];
+    increments_in_groups.push_back(next_index);    
+    tmp_index = next_index;
+  }
+
+  std::reverse(increments_in_groups.begin(),increments_in_groups.end());
   
+  std::vector<int> groups;
+  groups.push_back(increments_in_groups[0]);
+  for(int i=1;i<increments_in_groups.size();i++){
+    groups.push_back(increments_in_groups[i] - increments_in_groups[i-1]);
+  }
+
+  /**
+   *  Final Output
+   *
+   */
+  for(int i=0;i<groups.size();i++){
+    std::cout<<groups[i]<<"\t";
+  }
+  std::cout<<"\n";
 }
